dcc97a8780c388ef660141f4d3f82e7f807beeb4
Raul Viteri <raulviterip@gmail.com>
2025-11-17 18:07:06 -0500

    nuevo boton, cambio de color card

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Seleccionar el botón
-const button = document.querySelector("button");
+const button = document.getElementById("cambia_color");
+const buttonCard = document.getElementById("cambiar_color_card");
+
+const card = document.querySelector("body > div");
 
 // Agregar un evento al botón
 button.addEventListener("click", function () {
@@ -10,4 +13,13 @@ button.addEventListener("click", function () {
 
   // Mostrar alert
   alert("Has cambiado el background a " + colorHex);
+});
+
+// Agregar un evento al botón para cambiar el color del card
+buttonCard.addEventListener("click", function () {
+  const colorHex = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  
+  // Cambiar el color de fondo del card
+  card.style.backgroundColor = colorHex;
+  alert("Has cambiado el color de la card a " + colorHex);
 });
